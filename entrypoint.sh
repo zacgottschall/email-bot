f2a9258e30a4e2cfe9de8fa7507b42f776707bc6
ZACHARY GOTTSCHALL <zac.gottschall@xandr.com>
2020-11-21 20:22:31 -0600

    update entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+git pull
+[ -d "./venv" ] || python3 -m venv venv
 source venv/bin/activate
+pip3 install -r requirements.txt
+[ -f "./last_check" ] || touch "./last_check"
 python3 main.py
 deactivate
