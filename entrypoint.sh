6c04f614724b0a2f2fa1f7e43f554f00593663d3
ZACHARY GOTTSCHALL <zac.gottschall@xandr.com>
2020-11-22 10:38:41 -0600

    dockerize it

--- entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-touch /home/zacharygottschall/email-bot/test.txt
-/usr/bin/git pull
-source /home/zacharygottschall/email-bot/venv/bin/activate
-/home/zacharygottschall/email-bot/venv/bin/pip3 install -r /home/zacharygottschall/email-bot/requirements.txt
-/home/zacharygottschall/email-bot/venv/bin/python3 /home/zacharygottschall/email-bot/main.py
-touch /home/zacharygottschall/email-bot/test2.txt
-deactivate
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from os import getenv, remove
+from time import sleep
 from email_bot.utils import process_incoming_email
 from email_bot.models import IncomingEmail, EmailClient, OutgoingEmail
 from email_bot.exceptions import TaskException
@@ -11,17 +12,21 @@ if __name__ == "__main__":
     email_client = EmailClient(username, password)
 
     incoming_email: IncomingEmail
-    for incoming_email in email_client.get_new_emails():
-        try:
-            outgoing_email: OutgoingEmail = process_incoming_email(incoming_email)
-            email_client.send(outgoing_email, incoming_email.frm)
-            remove(outgoing_email.filename)
-        except TaskException as e:
-            email_client.send(OutgoingEmail(
-                subject="Something went wrong",
-                body="Contact zacharygottschall@gmail.com",
-            ), incoming_email.frm)
-            email_client.send(OutgoingEmail(
-                subject="Email Bot Error",
-                body="Input: {}\nError: {}".format(incoming_email.frm, str(e))
-            ), 'zacharygottschall@gmail.com')
+
+    while True:
+        for incoming_email in email_client.get_new_emails():
+            try:
+                outgoing_email: OutgoingEmail = process_incoming_email(incoming_email)
+                email_client.send(outgoing_email, incoming_email.frm)
+                remove(outgoing_email.filename)
+            except TaskException as e:
+                email_client.send(OutgoingEmail(
+                    subject="Something went wrong",
+                    body="Contact zacharygottschall@gmail.com",
+                ), incoming_email.frm)
+                email_client.send(OutgoingEmail(
+                    subject="Email Bot Error",
+                    body="Input: {}\nError: {}".format(incoming_email.frm, str(e))
+                ), 'zacharygottschall@gmail.com')
+        
+        sleep(10)
