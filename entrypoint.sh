3979e8e499f9f9a6458e701c84b872352bbf53d0
ZACHARY GOTTSCHALL <zac.gottschall@xandr.com>
2020-11-21 23:05:18 -0600

    fix file path

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,7 +3,7 @@
 touch /home/zacharygottschall/email-bot/test.txt
 /usr/bin/git pull
 source /home/zacharygottschall/email-bot/venv/bin/activate
-/usr/bin/pip3 install -r /home/zacharygottschall/email-bot/requirements.txt
-/usr/bin/python3 /home/zacharygottschall/email-bot/main.py
+/home/zacharygottschall/email-bot/venv/bin/pip3 install -r /home/zacharygottschall/email-bot/requirements.txt
+/home/zacharygottschall/email-bot/venv/bin/python3 /home/zacharygottschall/email-bot/main.py
 touch /home/zacharygottschall/email-bot/test2.txt
 deactivate
