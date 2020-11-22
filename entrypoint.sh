f96c1d1fa4e1fd8afe0b22da896acf24619cd302
ZACHARY GOTTSCHALL <zac.gottschall@xandr.com>
2020-11-21 20:46:05 -0600

    fix file path

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,7 @@
 
 git pull
 [ -d "./venv" ] || python3 -m venv venv
-[ -d "/email-bot-files" ] || mkdir email-bot-files
+[ -d "/email-bot-files" ] || mkdir /email-bot-files
 source venv/bin/activate
 pip3 install -r requirements.txt
 [ -f "./last_check" ] || touch "./last_check"
